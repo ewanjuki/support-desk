6cc673b2675877d2c4daca6fd2b51712a09fd0dc
Edwin Wanjuki <edwinwanjuki@gmail.com>
2023-02-08 07:14:07 +0300

    protected routes & authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,13 +64,27 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Get current user
+// @route  /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  };
+
+  res.status(200).json(user);
+});
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d'
+    expiresIn: "30d",
   });
 };
 
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
