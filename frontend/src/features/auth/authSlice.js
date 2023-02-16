17b5b279a361ecd453ec8d665c2c2af73b0d13aa
Edwin Wanjuki <edwinwanjuki@gmail.com>
2023-02-16 05:31:17 +0300

    logout user

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -37,6 +37,12 @@ export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
   console.log(user);
 });
 
+// Logout user
+export const logout = createAsyncThunk(
+  "auth/logout",
+  async () => await authService.logout()
+);
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -63,6 +69,9 @@ export const authSlice = createSlice({
         state.isError = true;
         state.user = null;
         state.message = action.payload;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
       });
   },
 });
